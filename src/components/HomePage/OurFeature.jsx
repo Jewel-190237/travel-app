fff26b3a21fcfdf90648122626229ef8d6e09f62
Jewel-190237 <jewel190237@gmail.com>
2024-08-23 00:21:27 +0600

    file uploaded

--- src/components/HomePage/OurFeature.jsx
+++ src/components/HomePage/OurFeature.jsx
@@ -1,10 +1,25 @@
+import Image from 'next/image';
 import React from 'react';
 
 const OurFeature = () => {
     return (
-        <div>
-            
-        </div>
+        <section>
+            <div className='mt-20 mx-auto w-full md:w-4/6 lg:w-1/3'>
+                <div className=''>
+                    <div className='flex flex-col'>
+                        <Image className='' src='/camp.svg' height={50} width={50} alt='feature'></Image>
+                    </div>
+                    <div>
+                        <h1 className=' text-6xl font-extrabold text-black '>Our Feature</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mt-20 w-full bg-cover bg-center bg-no-repeat p-0 md:p-4 lg:p-10 h-[220px] md:h-[300px] lg:h-[550px] rounded-xl" style={{ backgroundImage: "url('/feature-bg.png')" }}>
+
+            </div>
+
+        </section>
     );
 };
 
