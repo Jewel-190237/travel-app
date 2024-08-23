a01c8d2bba97af6bafcac5d25d7717cef8ca20ce
Jewel-190237 <jewel190237@gmail.com>
2024-08-23 16:16:57 +0600

    file uploaded

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -3,16 +3,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className='max-w-7xl mx-auto'>
             <footer className="footer bg-base-200 text-base-content p-10">
                 <aside>
                     <Image src='/hilink-logo.svg' alt='hlink' width={100} height={100} />
-
-                    ACME Industries Ltd.
-                    <br />
-                    Providing reliable tech since 1992
-                    <br />
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
 
                 </aside>
                 <nav>
@@ -28,6 +22,12 @@ const Footer = () => {
                     <a className="link link-hover">Contact</a>
                     <a className="link link-hover">Jobs</a>
                     <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Legal</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
                 </nav>
                 <nav>
                     <h6 className="footer-title">Legal</h6>
