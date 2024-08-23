5f0c27391d1014e535955693610e1513462c3110
Jewel-190237 <jewel190237@gmail.com>
2024-08-23 11:31:21 +0600

    file uploaded

--- src/components/HomePage/OurFeature.jsx
+++ src/components/HomePage/OurFeature.jsx
@@ -17,11 +17,10 @@ const OurFeature = () => {
 
             <div className="mt-10 mb-60 w-full bg-cover bg-center bg-no-repeat p-0 md:p-4 lg:p-10 h-[220px] md:h-[300px] lg:h-[550px] rounded-xl" style={{ backgroundImage: "url('/feature-bg.png')" }}>
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
-                    <div>
+                    <div className='hidden lg:flex'>
                         <Image className='rotate-12' src='/phone.png ' height={400} width={400} alt='phone'></Image>
                     </div>
                     <div className='col-span-2'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque facere laborum voluptate eum laudantium pariatur? Provident, soluta delectus quos in laudantium molestias hic, nihil deserunt illo voluptate possimus impedit architecto!
                     </div>
                 </div>
 
