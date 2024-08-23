b17e6368ef412a0d2acf76aed0244f9f7b670af6
Jewel-190237 <jewel190237@gmail.com>
2024-08-23 13:18:51 +0600

    file uploaded

--- src/components/HomePage/GetSection.jsx
+++ src/components/HomePage/GetSection.jsx
@@ -4,28 +4,27 @@ import React from 'react';
 const GetSection = () => {
     return (
         <section>
-            <div className="mt-10 mb-[800px] lg:mb-60 w-full bg-cover bg-center bg-no-repeat p-0 md:p-4 lg:p-10 h-[550px] rounded-2xl" style={{ backgroundImage: "url('/pattern.png')" }}>
+            <div className="mt-10 mb-[800px] lg:mb-60 w-full bg-cover bg-center bg-no-repeat p-0 md:p-4 lg:p-10 h-full lg:h-[600px] rounded-2xl" style={{ backgroundImage: "url('/pattern.png')" }}>
                 {/* <Image className='' src='/pattern.png' height={50} width={50} alt='feature'></Image> */}
-                <div className='flex flex-col md:flex-row items-center'>
-                    <div className='flex-1 p-10'>
+                <div className='flex flex-col items-center lg:items-start md:flex-row gap-10 p-4'>
+                    <div className='flex-1 pt-0 lg:pt-20'>
                         <h2 className='text-white font-bold text-6xl'>Get for <br /> free now!
                         </h2>
                         <p className='mt-10 text-white'>Available on iOS and Android</p>
-                        <div className='flex flex-col md:flex-row gap-8 mt-10'>
-                            <button className='flex items-center w-full rounded-full bg-white text-green-700 p-4'>
+                        <div className='flex flex-col lg:flex-row gap-8 mt-10'>
+                            <button className='flex items-center justify-center gap-3 w-full rounded-full bg-white text-green-700 p-4'>
                                 <Image className='' src='/apple.svg' height={20} width={20} alt='feature' />
                                 app store
                             </button>
-                            <button className='flex items-center w-full rounded-full  text-white border border-black'>
+                            <button className='flex items-center justify-center gap-3 w-full rounded-full  text-white border border-white p-4'>
                                 <Image className='' src='android.svg' height={20} width={20} alt='feature' />
                                 play store
                             </button>
 
                         </div>
                     </div>
-                    <div>
-                        <Image className='' src='/public/phones.png' height={50} width={50} alt='feature'></Image>
-
+                    <div className='flex-1 flex justify-center items-center lg:h-[850px] overflow-hidden -mt-40'>
+                        <Image className='object-center ' src='/phones.png' height={500} width={500} alt='feature'></Image>
                     </div>
                 </div>
 
